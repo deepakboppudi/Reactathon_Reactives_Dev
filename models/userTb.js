07ea5d18d7919c5cd7d1962e1d980a36285adc75
Deepak <deepak.boppudi@gmail.com>
2018-07-02 15:27:24 +0000

    Server changes for create and get user

---
+++ models/userTb.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+ var Schema = mongoose.Schema;
+
+ var userSchema = new Schema({
+   first_name: String,
+   last_name:   String,
+   email_id: String,
+   password: String,
+   user_type: Number
+ });
+
+ var User = mongoose.model('user_tbs', userSchema);
+
+
+ module.exports = User;
